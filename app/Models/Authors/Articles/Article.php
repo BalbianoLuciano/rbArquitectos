bdafc5d77113f6ae357d2801cef9d5f135e8d945
BalbianoLuciano <balbiano06@gmail.com>
2023-10-10 18:46:57 -0300

    Article model implemented

---
+++ app/Models/Authors/Articles/Article.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Authors\Articles;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Article extends Model
+{
+    use HasFactory;
+}

fb85bc21cfa5b641948b5324c6533a5b7cfd26c7
BalbianoLuciano <balbiano06@gmail.com>
2023-10-27 19:29:46 -0300

    clean code

--- resources/views/panel/companies/create.blade.php
+++ resources/views/panel/companies/create.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.admin')
 
 @section('content')
-    <h1>Create Company</h1>
+    <h1 class="py-3">Create Company</h1>
     <form action="{{ route('panel.companies.store') }}" method="POST">
         @csrf
         @include('panel.companies.form', ['buttonText' => 'Create Company'])

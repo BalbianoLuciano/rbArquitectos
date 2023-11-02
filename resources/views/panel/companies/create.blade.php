b45e320e4e300f2e12f005fdcdaa9cea98825223
BalbianoLuciano <balbiano06@gmail.com>
2023-11-02 19:11:11 -0300

    add enctype

--- resources/views/panel/companies/create.blade.php
+++ resources/views/panel/companies/create.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <h1 class="py-3">Create Company</h1>
-    <form action="{{ route('panel.companies.store') }}" method="POST">
+    <form action="{{ route('panel.companies.store') }}" method="POST" enctype="multipart/form-data">
         @csrf
         @include('panel.companies.form', ['buttonText' => 'Create Company'])
     </form>
--- resources/views/panel/companies/edit.blade.php
+++ resources/views/panel/companies/edit.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <h1>Edit Company</h1>
-    <form action="{{ route('panel.companies.update', $company) }}" method="POST">
+    <form action="{{ route('panel.companies.update', $company) }}" method="POST" enctype="multipart/form-data">
         @csrf
         @method('PUT')
         @include('panel.companies.form', ['buttonText' => 'Update Company'])

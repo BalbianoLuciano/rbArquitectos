afd3e658b39b0739ba0679bd8807fbe4f93dac88
BalbianoLuciano <balbiano06@gmail.com>
2023-11-13 10:55:16 -0300

    add dependencies

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,5 +15,6 @@
 @stop
 
 @section('js')
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-eOJFj2chUpjA5Q3WM5/FE4X8ZcLBCSg6uM6V5J1XfzkjiP4N7ER+/r27C3z4jzzK" crossorigin="anonymous"></script>
     <script> console.log('Hi!'); </script>
 @stop
